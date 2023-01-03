35ab07625225213bd184e44c7102c74ace99a9c3
JuanDavid <Juandbarrero@hotmail.com>
2023-01-03 17:00:35 -0500

    updated example

--- Thread/DoubleCore.py
+++ Thread/DoubleCore.py
@@ -4,7 +4,6 @@ from time import sleep
 
 led = Pin("LED", Pin.OUT)
 
-
 #sLock = _thread.allocate_lock()   #this is a semaphore 
 #sLock.acquire()   #Take semaphore 
 #sLock.release()   #Give semaphore 
@@ -15,11 +14,23 @@ def HelloWorld():
         print(f"Hola mundo {count}")
         count +=1
         sleep(1)
+        if count == 10:
+            _thread.exit()
 
         
 _thread.start_new_thread(HelloWorld,())
 
+def main():
+    while True:
+        print("Hola desde el core 0")
+        sleep(2)
+
+contador = 0
 while True:
     led.toggle()
+    contador += 1
     sleep(0.1)
+    if (contador == 100):
+        main()
+        
 
