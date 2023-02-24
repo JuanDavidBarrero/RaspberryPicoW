11f9d20f7822842af4b326b627cdd45bd1265fbb
Juan David Barrero L <Juandbarrero@hotmail.com>
2023-02-23 21:18:27 -0500

    key and iv added

--- AES/AES_encryption_decryption.py
+++ AES/AES_encryption_decryption.py
@@ -1,9 +1,14 @@
 from ucryptolib import aes
 import ujson
+import random
 
 person = {"name":"Juan","Pet":"Hades","Job":True}
 
-crypto = aes(b"hola mundo hades",1)
+#iv = byte_array = bytearray([0] * 16)
+iv = bytes([random.randint(0, 255) for _ in range(16)])
+key =  bytes([random.randint(0, 255) for _ in range(32)])
+
+crypto = aes(key,1,iv)
 data = ujson.dumps(person)
 data_bytes = data.encode()
 
@@ -13,12 +18,12 @@ padding = b'\x00' * padding_size
 padded_data = data_bytes + padding
 enc = crypto.encrypt(padded_data)
 
-print(f"the encrypt message is \n \t{enc} \n")
+print(f"the encrypt message is \n \t{enc.hex()} \n")
 
 data_enc = enc.hex()
 byte_str = bytes.fromhex(data_enc)
 
-crypto2 = aes(b"hola mundo hades",1)
+crypto2 = aes(key,1,iv)
 dec = crypto2.decrypt(byte_str)
 
 print(f"the decrypt message is \n \t {dec.decode('utf-8')}")
