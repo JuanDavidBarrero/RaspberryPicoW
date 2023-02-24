3c5d9f90eec1ba7934ddc1d3630e414c45c004a7
Juan David Barrero L <Juandbarrero@hotmail.com>
2023-02-23 21:09:27 -0500

    aes now with pack function

--- AES/AES_encryption_decryption.py
+++ AES/AES_encryption_decryption.py
@@ -7,12 +7,16 @@ crypto = aes(b"hola mundo hades",1)
 data = ujson.dumps(person)
 data_bytes = data.encode()
 
-enc = crypto.encrypt(data_bytes + b'\x00' * ((16 - (len(data_bytes) % 16)) % 16))
-print(f"the encrypt message is \n \t{enc.hex()} \n")
+block_size = 16
+padding_size = block_size - (len(data_bytes) % block_size)
+padding = b'\x00' * padding_size
+padded_data = data_bytes + padding
+enc = crypto.encrypt(padded_data)
+
+print(f"the encrypt message is \n \t{enc} \n")
 
 data_enc = enc.hex()
 byte_str = bytes.fromhex(data_enc)
-
 
 crypto2 = aes(b"hola mundo hades",1)
 dec = crypto2.decrypt(byte_str)
