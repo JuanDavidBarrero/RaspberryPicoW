1a79e863e78802cf5e6fb01409ee86f08dc6d53b
Juan David Barrero L <Juandbarrero@hotmail.com>
2023-02-23 21:06:56 -0500

    aes updated now with hex convertion

--- AES/AES_encryption_decryption.py
+++ AES/AES_encryption_decryption.py
@@ -8,9 +8,13 @@ data = ujson.dumps(person)
 data_bytes = data.encode()
 
 enc = crypto.encrypt(data_bytes + b'\x00' * ((16 - (len(data_bytes) % 16)) % 16))
-print(f"the encrypt message is \n \t{enc} \n")
+print(f"the encrypt message is \n \t{enc.hex()} \n")
+
+data_enc = enc.hex()
+byte_str = bytes.fromhex(data_enc)
+
 
 crypto2 = aes(b"hola mundo hades",1)
-dec = crypto2.decrypt(enc)
+dec = crypto2.decrypt(byte_str)
 
 print(f"the decrypt message is \n \t {dec.decode('utf-8')}")
